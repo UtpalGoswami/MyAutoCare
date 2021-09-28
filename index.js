7993862b667ba6a46a9488f4e6f8b0bd2c65a5d5
UG1993 <utpal.prydan@gmail.com>
2021-09-28 08:34:18 +0530

    Update

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ import { enableScreens } from 'react-native-screens';
 import { AppRegistry } from 'react-native';
 import App from './src/index';
 import { name as appName } from './app.json';
-enableScreens();
+enableScreens(); 
 AppRegistry.registerComponent(appName, () => App);
